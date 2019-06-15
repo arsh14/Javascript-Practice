55336fbd982731f106f64ae59e612950dbeabf80
arsh14 <arsh140898@gmail.com>
2019-06-16 01:20:06 +0530

    update

--- coursera js/script.js
+++ coursera js/script.js
@@ -56,3 +56,21 @@ console.log(company["name"]);
 company["stock of company"]=110;
 console.log(company);
 
+function multiply(x,y){
+	return x*y;
+}
+console.log(multiply(5,3));
+multiply.version="v1.0.0";
+console.log(multiply.version);
+console.log(multiply.toString());
+
+var a={x:7};
+var b=a;
+console.log(a);
+console.log(b);
+b.x=5;
+console.log("b.x updated");
+console.log(a);
+console.log(b);
+
+
